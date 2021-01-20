ca6d747ae43fa071c00505323f74bfe9c8e139ad
conorheins <conor.heins@gmail.com>
2021-01-20 21:09:25 +0100

    added utils file and likelihood generator func

---
+++ tmp.py
@@ -0,0 +1,31 @@
+import numpy as np
+from utils import obj_array, spm_dot, dot_likelihood
+from genmodel import generate_likelihood
+import itertools
+
+# %% Initialize constants
+"""
+Constant parameters of a focal agent's generative model, e.g. its number of neighbours
+"""
+
+num_neighbours = 2 # number of neighbours whose tweets our focal agent can read
+num_outcast_levels = 6 # 3 levels of observed discrepancy between focal agent and community, x 2 levels for the truth/falsity of the idea at hand
+
+"""
+Specify the mapping between hidden states (truth/falsity of Idea) and Hashtags - this matrix will be used to 'fill out' 
+the appropriate high-dimensional slices of the modality-specific A matrices later on.
+
+Note that how you set up this mapping determines:
+(1) the number of hashtags (i.e. the dimensionality of the hashtag modality) that an agent can tweet 
+(2) the focal agent's beliefs about the 'semantics' of the Idea--> Hashtag content mapping - how do Hashtags provide evidence for the Idea
+"""
+
+h_idea_mapping = np.array([[0.75, 0.25], 
+                      [0.25, 0.75]])
+
+num_H = h_idea_mapping.shape[0]# add an extra observation level to include the `null` observation
+
+h_control_mapping = np.array([[1, 0], 
+                      [0, 1]])
+                      
+A = generate_likelihood(h_idea_mapping, h_control_mapping, num_neighbours = num_neighbours, num_outcast_levels = num_outcast_levels)
---
+++ utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+def obj_array(shape):
+    return np.empty(shape, dtype=object)
+
+def spm_dot(X, x, dims_to_omit=None):
+    """ Dot product of a multidimensional array with `x`. The dimensions in `dims_to_omit` 
+    will not be summed across during the dot product
+
+    Parameters
+    ----------
+    - `x` [1D numpy.ndarray] - either vector or array of arrays
+        The alternative array to perform the dot product with
+    - `dims_to_omit` [list :: int] (optional)
+        Which dimensions to omit
+    
+    Returns 
+    -------
+    - `Y` [1D numpy.ndarray] - the result of the dot product
+    """
+
+    # Construct dims to perform dot product on
+    if is_arr_of_arr(x):
+        dims = (np.arange(0, len(x)) + X.ndim - len(x)).astype(int)
+    else:
+        dims = np.array([1], dtype=int)
+        x = to_arr_of_arr(x)
+
+    # delete ignored dims
+    if dims_to_omit is not None:
+        if not isinstance(dims_to_omit, list):
+            raise ValueError("`dims_to_omit` must be a `list` of `int`")
+        dims = np.delete(dims, dims_to_omit)
+        if len(x) == 1:
+            x = np.empty([0], dtype=object)
+        else:
+            x = np.delete(x, dims_to_omit)
+
+    # compute dot product
+    for d in range(len(x)):
+        s = np.ones(np.ndim(X), dtype=int)
+        s[dims[d]] = np.shape(x[d])[0]
+        X = X * x[d].reshape(tuple(s))
+        X = np.sum(X, axis=dims[d], keepdims=True)
+    Y = np.squeeze(X)
+
+    # check to see if `Y` is a scalar
+    if np.prod(Y.shape) <= 1.0:
+        Y = Y.item()
+        Y = np.array([Y]).astype("float64")
+
+    return Y
+
+def dot_likelihood(A,obs):
+
+    s = np.ones(np.ndim(A), dtype = int)
+    s[0] = obs.shape[0]
+    X = A * obs.reshape(tuple(s))
+    X = np.sum(X, axis=0, keepdims=True)
+    LL = np.squeeze(X)
+
+    # check to see if `LL` is a scalar
+    if np.prod(LL.shape) <= 1.0:
+        LL = LL.item()
+        LL = np.array([LL]).astype("float64")
+
+    return LL
+
+def is_arr_of_arr(arr):
+    return arr.dtype == "object"
+
+def to_arr_of_arr(arr):
+    if is_arr_of_arr(arr):
+        return arr
+    arr_of_arr = np.empty(1, dtype=object)
+    arr_of_arr[0] = arr.squeeze()
+    return arr_of_arr
