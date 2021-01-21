d7a500c09caf5ef2e80731df4b3cdd9c945f4230
conorheins <conor.heins@gmail.com>
2021-01-21 18:25:44 +0100

    specified precisions for each truth value

--- tmp.py
+++ tmp.py
@@ -1,8 +1,8 @@
+# %% Imports
 import numpy as np
 from utils import obj_array, spm_dot, dot_likelihood
 from genmodel import generate_likelihood
 import itertools
-
 # %% Initialize constants
 """
 Constant parameters of a focal agent's generative model, e.g. its number of neighbours
@@ -20,12 +20,15 @@ Note that how you set up this mapping determines:
 (2) the focal agent's beliefs about the 'semantics' of the Idea--> Hashtag content mapping - how do Hashtags provide evidence for the Idea
 """
 
-h_idea_mapping = np.array([[0.75, 0.25], 
-                      [0.25, 0.75]])
+h_idea_mapping = np.array([[0.6, 0.4], 
+                      [0.4, 0.6]])
+
+true_false_precisions = [5.0, 5.0]
 
 num_H = h_idea_mapping.shape[0]# add an extra observation level to include the `null` observation
 
 h_control_mapping = np.array([[1, 0], 
                       [0, 1]])
                       
-A = generate_likelihood(h_idea_mapping, h_control_mapping, num_neighbours = num_neighbours, num_outcast_levels = num_outcast_levels)
+A = generate_likelihood(h_idea_mapping, h_control_mapping, true_false_precisions, num_neighbours = num_neighbours, num_outcast_levels = num_outcast_levels)
+# %%
