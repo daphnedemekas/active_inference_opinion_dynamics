5d1e1376e98dd554cb455cd41b91821694f00004
Daphne Demekas <zcahdde@ucl.ac.uk>
2021-04-07 17:23:53 +0100

    add KL divergence metrics

--- network_construction.py
+++ network_construction.py
@@ -17,7 +17,6 @@ def agent_loop(agent, observations = None, initial = False, initial_action = Non
     who_i_looked_at = int(action[-1]+1)
     what_they_tweeted = observations[int(action[-1])+1]
 
-
     if initial == True:
         action = agent.action
     return action, qs
@@ -26,19 +25,15 @@ def multi_agent_loop(T, agents, agent_neighbours):
     actions = []
     timestep = 0
     action = None
-
     observations = []
 
-    def reset_observations(agent):
-        o = []
-        o.append(None)
-        for n in range(agent.genmodel.num_neighbours):
-            o.append(0)
-        o.append(None)
-        return o
-
     for agent in agents:
-        observations.append(reset_observations(agent))
+        obs = []
+        obs.append(None)
+        for n in range(agent.genmodel.num_neighbours):
+            obs.append(0)
+        obs.append(None)
+        observations.append(obs)
 
     initial = True
     all_actions = obj_array((T,N,))
@@ -48,11 +43,10 @@ def multi_agent_loop(T, agents, agent_neighbours):
 
     for t in range(T):
         #print(str(t) + "/" + str(T))
-        #initial_actions = [[0,1],[1,0],[1,0],[0,2],[1,3],[1,4]]
         actions = []
         for i in range(len(agents)):
             action, belief = agent_loop(agents[i], observations[i], initial)
-            all_actions[t,i] = action
+            all_actions[t,i] = action[-2:]
             all_beliefs[t,i] = belief
             actions.append(action)
 
@@ -73,22 +67,20 @@ def multi_agent_loop(T, agents, agent_neighbours):
         #    make_plots(all_actions, all_beliefs,p,timestep)
     return all_actions, all_beliefs, all_observations
 
-def color_dict(value):
-    if value < 0.5:
-        return "blue"
-    else:
-        return "red"
+
+
 def make_plots(all_actions, agent_own_beliefs,p,T):
-
-    
-
     tweet_history = np.zeros((N, T))
+
+    def color_dict(value):
+        if value < 0.5:
+            return "blue"
+        else:
+            return "red"
 
     for t in range(T):
         for n in G.nodes():
             tweet_history[n,t] = all_actions[t][n][-2]
-
-    
     for a in range(N):
         data = agent_own_beliefs[a][:,0]
         plt.plot(data, color = color_dict(data[-1]), label = "beliefs in idea 1")
@@ -96,23 +88,13 @@ def make_plots(all_actions, agent_own_beliefs,p,T):
     plt.title("Connectedness of graph: " +str(p))
     plt.ylabel("Belief that idea is True")
     plt.xlabel("Time")
-    #time.sleep(5)
     plt.show()
-    #plt.savefig("p = " + str(p))
-
-
 
     #sns.heatmap(tweet_history, cmap='gray', vmax=1., vmin=0., cbar=True)
     #plt.show()
 
-
-
-
-
-
-N = 4 # total number of agents
-idea_levels = 2 # the levels of beliefs that agents can have about the idea (e.g. 'True' vs. 'False', in case `idea_levels` ==2)
-num_H = 2 #the number of hashtags, or observations that can shed light on the idea
+def KL_div(array1_0, array1_1, array2_0, array2_1):
+    return array1_0 * np.log(array1_0 / array2_0) + array1_1 * np.log(array1_1 / array2_1)
 
 def inference_loop(G,N):
     try:
@@ -125,60 +107,84 @@ def inference_loop(G,N):
     
     return all_actions, all_beliefs, all_observations, agents, agent_neighbours
 
-p_vec = np.linspace(0.3,1,5) # different levels of random connection parameter in Erdos-Renyi random graphs
-num_trials = 1 # number of trials per level of the ER parameter
-T = 5
 
-#fig, axs = plt.subplots(len(p_vec)/2, len(p_vec)/2)
-j = 0
-for param_idx, p in enumerate(p_vec):
-    print("p is" + str(p))
-    if param_idx % len(p_vec)/2:
-        j += 1
-
-    for trial_i in range(num_trials):
+def get_belief_metrics(all_beliefs, agents, agent_neighbours,T):
+    N = len(agents)
+    agent_own_beliefs_per_timestep = np.array([[belief[0] for belief in all_beliefs[:,a]] for a in range(N)]) # (N,T,2)
         
-        G = nx.fast_gnp_random_graph(N,p) # create the graph for this trial & condition
+    all_neighbour_perceptions = np.zeros((N,N-1,T,2))
+    
+    #KL divergences between agent's beliefs and their beliefs about neighbour's beliefs
+    KLD_inter_beliefs = np.zeros((N,N-1,T))
 
-        all_actions, all_beliefs, all_observations, agents, agent_neighbours = inference_loop(G,N)
+    #KL_divergences between agents' beliefs
+    KLD_intra_beliefs = np.zeros((N,N,T))
 
-        agent_own_beliefs_per_timestep = np.array([[belief[0] for belief in all_beliefs[:,a]] for a in range(len(agents))]) # (N,T,2)
-        
-        all_neighbour_perceptions = np.zeros((N,N-1,T,2))
-        for a in range(len(agents)):
-            agent_p = []
-            for i, n in enumerate(agent_neighbours[a]):
-                all_neighbour_perceptions[a,i] = np.array([belief[n] for belief in all_beliefs[:,a]])
+    for a in range(len(agents)):
+        agent_p = []
+        for i, n in enumerate(agent_neighbours[a]):
+            all_neighbour_perceptions[a,i] = np.array([belief[n] for belief in all_beliefs[:,a]])
+            KLD_inter_beliefs[a,i] = KL_div(all_neighbour_perceptions[a,i][:,0], all_neighbour_perceptions[a,i][:,1], agent_own_beliefs_per_timestep[a][:,0] , agent_own_beliefs_per_timestep[a][:,1])
 
-        agent_hashtag_beliefs_per_timestep = [[belief[-2] for belief in all_beliefs[:,a]] for a in range(len(agents))] # (N,T,2)
-        agent_who_idx_beliefs_per_timestep = [[belief[-1] for belief in all_beliefs[:,a]] for a in range(len(agents))] # (N,T,2)
+    for a in range(len(agents)):
+        for n in range(len(agents)):
+            KLD_intra_beliefs[a,n] = KL_div(agent_own_beliefs_per_timestep[a][:,0], agent_own_beliefs_per_timestep[a][:,1], agent_own_beliefs_per_timestep[n][:,0], agent_own_beliefs_per_timestep[n][:,1])
 
-        make_plots(all_actions, agent_own_beliefs_per_timestep,p,T)
+    agent_hashtag_beliefs_per_timestep = [[belief[-2] for belief in all_beliefs[:,a]] for a in range(N)] # (N,T,2)
+    agent_who_idx_beliefs_per_timestep = [[belief[-1] for belief in all_beliefs[:,a]] for a in range(N)] # (N,T,2)
 
-        #proportion of agents believing in idea 1 at the final timestep 
-        final_timestep_beliefs = [0 if agent_own_beliefs_per_timestep[:,-1][a][0] < 0.5 else 1 for a in range(len(agents))]
-        idea_1_believers = sum(final_timestep_beliefs) 
-        idea_0_believers = len(final_timestep_beliefs) - idea_1_believers
+    #proportion of agents believing in idea 1 at the final timestep 
+    final_timestep_beliefs = [0 if agent_own_beliefs_per_timestep[:,-1][a][0] < 0.5 else 1 for a in range(N)]
+    idea_1_believers = sum(final_timestep_beliefs) 
+    idea_0_believers = len(final_timestep_beliefs) - idea_1_believers
+    belief_proportions = [idea_0_believers/N, idea_1_believers/N]
 
-        belief_proportions = [idea_0_believers/N, idea_1_believers/N]
+    #rate of change of belief per agent
+    #MAKE THESE KL DIVERGENCES
+    difference_of_beliefs_per_timestep = np.diff(agent_own_beliefs_per_timestep)
+    belief_differences = [np.sum(b) for b in difference_of_beliefs_per_timestep]
+    belief_differences_normalised = belief_differences / np.sum(np.abs(np.array(belief_differences)))
 
-        print("belief proportions")
-        print(belief_proportions)
-        #rate of change of belief per agent
-        strength_of_beliefs_per_timestep = np.diff(agent_own_beliefs_per_timestep)
-        total_strengths = [np.sum(b) for b in strength_of_beliefs_per_timestep]
-        print("belief strengths")
-        total_strengths_normalised = total_strengths / np.sum(np.abs(np.array(total_strengths)))
-        print(total_strengths_normalised)
-
-        idea_1_strength = sum(x for x in total_strengths_normalised if x > 0 )
-        idea_0_strength = - sum(x for x in total_strengths_normalised if x <0 )
-
-        plt.bar(["idea 0","idea 1"],belief_proportions)
-        plt.show()
-        plt.bar(["idea 0","idea 1"],[idea_0_strength, idea_1_strength])
-        plt.show()
+    return KLD_inter_beliefs, KLD_intra_beliefs, belief_proportions, belief_differences_normalised, agent_hashtag_beliefs_per_timestep, agent_who_idx_beliefs_per_timestep
 
 
+def get_action_metrics(all_actions, N,T):
+    all_actions = np.array(all_actions) # shape is T, N, 2
+    agent_actions_per_timestep = np.array([[action[0] for action in all_actions[:,a]] for a in range(N)]) # (N,T,2)
+    
+    agent_tweet_proportions = np.zeros((N,2))
 
+    for a in range(N):
+        hashtag1 = sum(agent_actions_per_timestep[a])
+        hashtag2 = len(agent_actions_per_timestep[a]) - hashtag1
+        agent_tweet_proportions[a] = [hashtag1/T, hashtag2/T]
 
+    return agent_tweet_proportions
+
+if __name__ == '__main__':
+
+    N = 4 # total number of agents
+    idea_levels = 2 
+    num_H = 2
+
+    p_vec = np.linspace(0.7,1,1) # different levels of random connection parameter in Erdos-Renyi random graphs
+    num_trials = 1 # number of trials per level of the ER parameter
+    T = 50
+
+    #fig, axs = plt.subplots(len(p_vec)/2, len(p_vec)/2)
+    for param_idx, p in enumerate(p_vec):
+        print("p is" + str(p))
+
+        for trial_i in range(num_trials):
+            
+            G = nx.fast_gnp_random_graph(N,p) # create the graph for this trial & condition
+
+            all_actions, all_beliefs, all_observations, agents, agent_neighbours = inference_loop(G,N)
+
+            KLD_inter_beliefs, KLD_intra_beliefs, belief_proportions, belief_differences_normalised, _, _ = get_belief_metrics(all_beliefs, agents, agent_neighbours,T)
+            #make_plots(all_actions, agent_own_beliefs_per_timestep,p,T)
+            
+            tweet_proportions = get_action_metrics(all_actions, N, T)
+            print(tweet_proportions)
+            #plt.imshow(KLD_intra_beliefs[:,:,-1], cmap = 'gray')
+            #plt.show()
