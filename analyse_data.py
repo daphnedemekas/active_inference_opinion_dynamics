d1b038cbfd348e14a7d2ef0182727c879c5b94f3
Daphne Demekas <zcahdde@ucl.ac.uk>
2021-04-18 12:58:18 +0100

    notebook with widgets

--- analyse_data.py
+++ analyse_data.py
@@ -1,107 +1,101 @@
-# %% Set up generative model
+# %% imports
+%matplotlib widget
 
+import ipywidgets as widgets
 import numpy as np
 import matplotlib.pyplot as plt 
-from matplotlib import figure 
-from ipywidgets import interact 
-from matplotlib.widgets import Slider
 import pandas as pd 
+from IPython.display import display
+# %% define values and get data file
+
+# Change matplotlib backend
+num_agent_values = [5,10,15]
+
+connectedness_values = [0.2,0.5,0.8]
+ranges = [[1,2],[1,5],[1,9],[6,7],[6,10]]
+str_ranges = ['[1,2]','[1,5]','[1,9]','[6,7]','[6,10]']
 
 df = pd.read_pickle("param_data.pkl")
-print(df)
+#param_results = np.load('results/params 2.npz',allow_pickle = True)['arr_0']
 
-def get_ratio_from_parameters(c, e_l, e_u, env_l, env_u, b_l, b_u):
-    indices = (c, e_l, e_u, env_l, env_u, b_l, b_u)
+# %% function to access the real parameters from the simulation
+
+def get_real_precisions(n,c,l,u,r1, r2, r3):
+    n_i = num_agent_values.index(n)
+    c_i = connectedness_values.index(c)
+    all_agent_params = param_results[n_i,c_i,r1,r2,r3]
+    return all_agent_params
+# %% function to access the cluster ratio from the inputted params
+
+def get_ratio_from_parameters(n, c, r1, r2, r3):
+    #first need to rount to the closest valuable option 
+    indices = (n, c, r1[0], r1[1], r2[0], r2[1], r3[0], r3[1])
     return df[indices]
 
+# %% make widgets
 
-connectedness_values = [0.2,0.4]
-lower_bounds = [1,2]
-upper_bounds = [i+j for i in lower_bounds for j in range(1)]
-fig = plt.figure(figsize=(6, 4))
-ax = fig.add_subplot(111)
-fig.subplots_adjust(bottom=0.2, top=0.75)
+n_d = widgets.Dropdown(value = 5, options = num_agent_values, description = "number of agents")
+c_d = widgets.Dropdown(value = 0.5, options = connectedness_values, description = 'graph connectedness')
 
-#adjustable range values
-ax_range = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_range.spines['top'].set_visible(True)
-ax_range.spines['right'].set_visible(True)
+r1_d = widgets.Dropdown(value = '[1,5]', options = str_ranges, description = 'precision ranges')
+r2_d = widgets.Dropdown(value = '[1,5]', options = str_ranges, description = 'env precision ranges')
+r3_d = widgets.Dropdown(value = '[1,5]', options = str_ranges, description = 'belief precision ranges') 
 
-#adjustable connectedness values
-ax_c = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_c.spines['top'].set_visible(True)
-ax_c.spines['right'].set_visible(True)
+# %% update function
+#here i want to make something where you can click a button and see what the parameters really were 
 
-#adjustable idea precision values
-ax_env_l = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_env_l.spines['top'].set_visible(True)
-ax_env_l.spines['right'].set_visible(True)
+# %% initialize plot
 
-#adjustable idea precision values
-ax_env_u = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_env_u.spines['top'].set_visible(True)
-ax_env_u.spines['right'].set_visible(True)
+fig, ax = plt.subplots(figsize=(6, 4))
+values = []
+for r in ranges:
+    values.append(get_ratio_from_parameters(n_d.value, c_d.value, r, ranges[0], ranges[0]))
+plt.plot(str_ranges, values)
+ax.set_ylim([0,1])
+ax.grid(True)
+plt.show()
 
-#adjustable idea precision values
-ax_b_l = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_b_l.spines['top'].set_visible(True)
-ax_b_l.spines['right'].set_visible(True)
+# %% update function
+output = widgets.Output()
+@output.capture
+def n_update(change):
+    print("Fuck u")
+    with output:
+        replot(change.new, c_d.value, r2_d.value, r3_d.value)
+@output.capture
+def c_update(change):
+    replot(n_d.value, change.new, r2_d.value, r3_d.value)
+@output.capture
+def r2_update(change):
+    replot(n_d.value, c_d.value, change.new, r3_d.value)
+@output.capture
+def r3_update(change):
+    print("Hello?")
+    plt.plot([1,2],[3,4])
+    plt.show()
+    replot(n_d.value, c_d.value, r2_d.value, change.new)
 
-#adjustable idea precision values
-ax_b_u = fig.add_axes([0.3, 0.85, 0.4, 0.05])
-ax_b_u.spines['top'].set_visible(True)
-ax_b_u.spines['right'].set_visible(True)
-
-
-s_u = Slider(ax=ax_range, label='precision upper bound', valmin=min(upper_bounds), valmax=max(upper_bounds),facecolor='#cc7000')
-
-s_c = Slider(ax=ax_c, label='graph connectedness', valmin=0, valmax=0.4, facecolor='#cc7000')
-
-s_env_l = Slider(ax=ax_env_l, label='idea precision lower bound', valmin=min(lower_bounds), valmax=max(lower_bounds), facecolor='#cc7000')
-s_env_u = Slider(ax=ax_env_u, label='idea precision upper bound', valmin=min(upper_bounds), valmax=max(upper_bounds), facecolor='#cc7000')
-s_b_l = Slider(ax=ax_b_l, label='belief precision', valmin=min(lower_bounds), valmax=max(lower_bounds), facecolor='#cc7000')
-s_b_u = Slider(ax=ax_b_u, label='belief precision range', valmin=min(upper_bounds), valmax=max(upper_bounds), facecolor='#cc7000')
-
-
-u_0 = 1
-c_0 = 0.4
-env_l_0 = lower_bounds[0]
-env_u_0 = upper_bounds[-1]
-b_l_0 = lower_bounds[0]
-b_u_0 = upper_bounds[-1]
-
-y_axis = []
-for precision_l in lower_bounds:
-    print(c_0, precision_l, precision_l+1, env_l_0, env_u_0, b_l_0, b_u_0)
-    ratio = get_ratio_from_parameters(c_0, precision_l, u_0, env_l_0, env_u_0, b_l_0, b_u_0)
-    print("ratio")
-    print(ratio)
-    y_axis.append(ratio)
-
-
-
-graph, = ax.plot(lower_bounds, y_axis, linewidth = 2.5)
-
-# Update values
-def update(val):
-    s_u = s_u.val
-    s_c = s_c.val
-    s_env_l = s_env_l.val
-    s_env_u = s_env_u.val
-    s_b_l = s_b_l.val
-    s_b_u = s_b_u.val
+def replot(n, c, env_r, b_r):
+    #output.clear_output()
+    r2 = ranges[str_ranges.index(env_r)]
+    r3 = ranges[str_ranges.index(b_r)]
     new_data = []
-    for l in lower_bounds:
-        new_data.append(get_ratio_from_parameters(c_0, lower_bounds, r_0, env_l_0, env_u_0, b_l_0, b_u_0))
+    for r in ranges:
+        new_data.append(get_ratio_from_parameters(n, c, r, env_r, b_r))
+    with output:
+        plt.plot(str_ranges, new_data)
+        plt.show()
+        display(output)
 
-    graph.set_data(lower_bounds, new_data)
-    fig.canvas.draw_idle()
+# %% define the observations
+n_d.observe(n_update, names='value')
+c_d.observe(c_update, names='value')
+r2_d.observe(r2_update, names='value')
+r3_d.observe(r3_update, names='value')
 
-s_u.on_changed(update)
-s_c.on_changed(update)
-s_env_l.on_changed(update)
-s_env_u.on_changed(update)
-s_b_l.on_changed(update)
-s_b_u.on_changed(update)
-
-#ax.show()
+display(n_d)
+display(c_d)
+display(r1_d)
+display(r2_d)
+display(r3_d)
+# %%
