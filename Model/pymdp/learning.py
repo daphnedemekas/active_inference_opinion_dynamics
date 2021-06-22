f66ef3ef8220c00a8364d019699de470a07c177d
Sophia Wilhelm-Demekas <sophiawd@Sophias-MacBook-Pro-2.local>
2021-06-22 11:42:42 -0500

    learning file

---
+++ Model/pymdp/learning.py
@@ -0,0 +1,23 @@
+import numpy as np
+from . import utils
+
+def update_E(action_id, control_factor_id, policies, learning_rate = 0):
+    # print("action id")
+    # print(action_id)
+    num_policies = len(policies)
+    E_increment = np.zeros(num_policies)
+
+    # turn policies (list of arrays) into a single array
+    policy_array = np.stack(policies, 0).squeeze()
+
+    indices_to_bump = (policy_array[:,control_factor_id] == action_id)
+
+    E_increment[indices_to_bump] = learning_rate
+
+    return E_increment
+
+
+
+
+
+
